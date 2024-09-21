99aa35714b128bd997741024bbfed90ab2702116
Nigel Nelson <nigeln@nvidia.com>
2024-09-21 11:05:13 -0700

    Update checkpoint dir
    Signed-off-by: Nigel Nelson <nigeln@nvidia.com>

--- batch_imitate_episodes.py
+++ batch_imitate_episodes.py
@@ -41,7 +41,7 @@ def main(task, json_config):
 
     camera_names = task_config['camera_names']
 
-    checkpoint_dir = f"/lustre/fs11/portfolios/healthcareeng/users/nigeln/rgb-act-weights/real_data/{wandb_id}"
+    checkpoint_dir = f"/lustre/fsw/portfolios/healthcareeng/users/nigeln/rgb-act-weights/real_data/{wandb_id}"
     args = {
         'lr': json_config.learning_rate,  # You might want to make this configurable
         'num_queries': json_config.chunk_size,  # You might want to make this configurable
